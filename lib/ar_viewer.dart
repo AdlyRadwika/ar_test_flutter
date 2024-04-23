38243d68d3570b6688aac68be2587479d54cf4c0
AdlyRadwika <aradwika@gmail.com>
2024-04-23 22:04:46 +0700

    Refactor: adjust the panning issue and remove useless functions

--- lib/ar_viewer.dart
+++ lib/ar_viewer.dart
@@ -5,6 +5,7 @@ import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin/models/ar_anchor.dart';
 import 'package:ar_flutter_plugin/models/ar_node.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart';
@@ -18,80 +19,38 @@ class ARViewer extends StatefulWidget {
 }
 
 class _ARViewerState extends State<ARViewer> {
-  late ARSessionManager arSessionManager;
-  late ARObjectManager arObjectManager;
-  ARNode? webObjectNode;
-  bool isAdd = false;
-  double scaleValue = 0;
+  ARSessionManager? _arSessionManager;
+  late ARObjectManager? _arObjectManager;
+  ARAnchorManager? _arAnchorManager;
+  ARNode? _webObjectNode;
 
-  @override
-  void dispose() {
-    arSessionManager.dispose();
-    super.dispose();
-  }
+  List<ARAnchor> _anchors = [];
+  bool _isAdd = true;
+  late Quaternion _cumulativeRotation;
 
-  void onARViewCreated(
+  void _onARViewCreated(
     ARSessionManager arSessionManager,
     ARObjectManager arObjectManager,
     ARAnchorManager arAnchorManager,
     ARLocationManager arLocationManager,
   ) {
-    this.arSessionManager = arSessionManager;
-    this.arObjectManager = arObjectManager;
+    _arSessionManager = arSessionManager;
+    _arObjectManager = arObjectManager;
+    _arAnchorManager = arAnchorManager;
 
-    this.arSessionManager.onInitialize(
-          showFeaturePoints: false,
-          showPlanes: true,
-          customPlaneTexturePath: "assets/triangle.png",
-          showWorldOrigin: true,
-          showAnimatedGuide: false,
-          handlePans: true,
-          handleRotation: true,
-        );
-    this.arObjectManager.onInitialize();
-
-    this.arObjectManager.onPanStart = _onPanStarted;
-    this.arObjectManager.onPanChange = _onPanChanged;
-    this.arObjectManager.onPanEnd = _onPanEnded;
-    this.arObjectManager.onRotationStart = _onRotationStarted;
-    this.arObjectManager.onRotationChange = _onRotationChanged;
-    this.arObjectManager.onRotationEnd = _onRotationEnded;
+    _arSessionManager?.onInitialize(
+      showFeaturePoints: false,
+      showPlanes: false,
+      showWorldOrigin: false,
+      showAnimatedGuide: false,
+    );
+    _arObjectManager?.onInitialize();
 
     _addARObjectInFrontOfCamera();
   }
 
-  _onPanStarted(String nodeName) {
-    debugPrint("Started panning node $nodeName");
-  }
-
-  _onPanChanged(String nodeName) {
-    debugPrint("Continued panning node $nodeName");
-  }
-
-  _onPanEnded(String nodeName, Matrix4 newTransform) {
-    debugPrint("Ended panning node $nodeName");
-    // final pannedNode = webObjectNode;
-
-    // pannedNode?.transform = newTransform;
-  }
-
-  _onRotationStarted(String nodeName) {
-    debugPrint("Started rotating node $nodeName");
-  }
-
-  _onRotationChanged(String nodeName) {
-    debugPrint("Continued rotating node $nodeName");
-  }
-
-  _onRotationEnded(String nodeName, Matrix4 newTransform) {
-    debugPrint("Ended rotating node $nodeName");
-    // final rotatedNode = webObjectNode;
-
-    // rotatedNode?.transform = newTransform;
-  }
-
   void _addARObjectInFrontOfCamera() async {
-    final cameraPose = await arSessionManager.getCameraPose();
+    final cameraPose = await _arSessionManager?.getCameraPose();
     if (cameraPose != null) {
       final translation = cameraPose.getTranslation();
       final rotation = cameraPose.getRotation();
@@ -110,51 +69,66 @@ class _ARViewerState extends State<ARViewer> {
         transformation: newTransform,
       );
 
-      arObjectManager.addNode(newNode);
-      webObjectNode = newNode;
+      _arObjectManager?.addNode(newNode);
+      _webObjectNode = newNode;
     }
   }
 
   void _onPanUpdate(DragUpdateDetails details) {
-    if (webObjectNode != null) {
-      // Calculate the rotation based on the pan delta
-      final dx = details.delta.dx;
-      final dy = details.delta.dy;
+    if (_webObjectNode != null) {
+      final touchDelta = details.delta;
 
-      // Calculate the rotation axis (the camera's up vector)
-      final upVector = Vector3(0, 1, 0);
+      final rotationAmountX = touchDelta.dy * 0.01;
+      final rotationAmountY = touchDelta.dx * 0.01;
 
-      // Calculate the rotation angle based on pan deltas
-      final rotationAngleX = dx * 0.01;
-      final rotationAngleY = dy * 0.01;
+      final rotationX = Quaternion.axisAngle(Vector3(1, 0, 0), rotationAmountX);
+      final rotationY = Quaternion.axisAngle(Vector3(0, 1, 0), rotationAmountY);
 
-      // Convert current rotation to a quaternion
-      final currentRotation = webObjectNode!.rotation;
-      final currentQuaternion = Quaternion.fromRotation(currentRotation);
+      final totalRotation = rotationX * rotationY;
 
-      // Apply rotation around the camera's up vector
-      final rotationX = Quaternion.axisAngle(upVector, rotationAngleX);
-      final rotationY = Quaternion.axisAngle(upVector, rotationAngleY);
+      _cumulativeRotation *= totalRotation;
 
-      // Combine rotations
-      final newRotation = rotationX * rotationY * currentQuaternion;
-
-      // Set the new rotation back to the object
-      webObjectNode!.rotationFromQuaternion = newRotation;
+      _webObjectNode!.rotationFromQuaternion = _cumulativeRotation;
     }
   }
 
-  Future onWebObjectAtButtonPressed() async {
+  Future<void> _onWebObjectAtButtonPressed() async {
     setState(() {
-      isAdd = !isAdd;
+      _isAdd = !_isAdd;
     });
 
-    if (webObjectNode != null) {
-      arObjectManager.removeNode(webObjectNode!);
-      webObjectNode = null;
+    if (_webObjectNode != null) {
+      _arObjectManager?.removeNode(_webObjectNode!);
+      _webObjectNode = null;
     } else {
       _addARObjectInFrontOfCamera();
     }
+  }
+
+  Future<void> _onRemoveEverything() async {
+    setState(() {
+      for (var anchor in _anchors) {
+        _arAnchorManager?.removeAnchor(anchor);
+      }
+      _anchors = [];
+      if (_webObjectNode != null) {
+        _arObjectManager?.removeNode(_webObjectNode!);
+        _webObjectNode = null;
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _cumulativeRotation = Quaternion.identity();
+  }
+
+  @override
+  void dispose() {
+    _arSessionManager?.dispose();
+    super.dispose();
   }
 
   @override
@@ -164,13 +138,25 @@ class _ARViewerState extends State<ARViewer> {
       body: GestureDetector(
         onPanUpdate: _onPanUpdate,
         child: ARView(
-          onARViewCreated: onARViewCreated,
+          onARViewCreated: _onARViewCreated,
           planeDetectionConfig: PlaneDetectionConfig.horizontalAndVertical,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: onWebObjectAtButtonPressed,
-        child: Icon(isAdd ? Icons.remove : Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _onRemoveEverything,
+            heroTag: null,
+            child: const Icon(Icons.delete),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: _onWebObjectAtButtonPressed,
+            child: Icon(_isAdd ? Icons.visibility_off : Icons.visibility),
+          ),
+        ],
       ),
     );
   }
